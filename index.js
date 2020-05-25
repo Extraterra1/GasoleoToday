0b1451bff83286f03b7a40dfed67617b6f83efe7
artxE <extraterra1@gmail.com>
2020-05-25 12:06:08 +0100

    error handling

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get("/", async (req, res) => {
   const html = response.data;
   const $ = cheerio.load(html);
   const text = $("#divConteudo p").text();
+  const initialDate = $("#divConteudo p strong")
+    .first()
+    .text()
+    .replace(/\./g, "/")
+    .split(" a ");
   const x = text.match(/\d+(?:\.\d+)?/g);
   let prices = [];
   x.forEach((e) => {
@@ -22,12 +27,9 @@ app.get("/", async (req, res) => {
   const curPrices = prices.slice(0, 3);
   const nextWeekPrices = prices.slice(3, 6);
 
-  const week = moment().isoWeek();
-  const startCurWeek = moment()
-    .day("Monday")
-    .isoWeek(week)
-    .format("DD/MM/YYYY");
-  const endCurWeek = moment().day("Sunday").isoWeek(week).format("DD/MM/YYYY");
+  const week = moment(initialDate[0], "DD/MM/YYYY").isoWeek();
+  const startCurWeek = initialDate[0];
+  const endCurWeek = initialDate[1];
   const startNextWeek = moment()
     .day("Monday")
     .isoWeek(week + 1)
@@ -49,6 +51,10 @@ app.get("/", async (req, res) => {
 
 app.get("/como-funciona", (req, res) => {
   res.render("landing");
+});
+
+app.get("*", (req, res) => {
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, () => {
